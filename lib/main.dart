3ea0c13a91b44a3c35298bad00e570c7385bc40a
Davi Arndt <davi-arndt@outlook.com>
2020-04-09 18:57:37 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lista_anotacoes/screens/lista_anotacoes.dart';
+
+void main() => runApp(KeepIt());
+
+class KeepIt extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ListaAnotacoes(),
+    );
+  }
+}
+
